1fa92b3dedf91ae540cbc2f608a3344648ee0072
João Paulo Fricks <jpfricks@gmail.com>
2022-01-06 21:58:29 -0300

    challenge completed 🚀

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Flex } from "@chakra-ui/react";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       <Flex direction="column">
         <Header />
+        <Banner />
       </Flex>
     </div>
   );
