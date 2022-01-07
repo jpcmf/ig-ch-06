25a3474d378a5368c79075666b57c63d5618e183
João Paulo Fricks <jpfricks@gmail.com>
2022-01-06 21:28:03 -0300

    challenge completed 🚀

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,33 @@
+import { Flex, Grid, Image } from "@chakra-ui/react";
+
 export default function Header() {
   return (
-    <header>
-      <img src="/logo.svg" alt="logo" />
-    </header>
+    <Flex
+      bg="white"
+      w="100%"
+      as="header"
+      mx="auto"
+      px="1rem"
+      h={["50px", "100px"]}
+      align="center"
+      justify="center"
+    >
+      <Grid
+        h="100%"
+        w="100%"
+        mx="auto"
+        maxW="1160px"
+        alignItems="center"
+        templateColumns="repeat(3, 1fr)"
+        justifyContent="center"
+      >
+        <Image
+          src="/logo.svg"
+          alt="Worldtrip"
+          justifySelf="center"
+          gridColumn="2"
+        />
+      </Grid>
+    </Flex>
   );
 }
