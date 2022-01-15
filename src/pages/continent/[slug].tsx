48c84898c70ed0feb27640f801318321ee3f6997
João Paulo Fricks <jpfricks@gmail.com>
2022-01-14 23:35:16 -0300

    challenge completed 🚀

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,7 +1,8 @@
 import { Flex } from "@chakra-ui/react";
+import Cities from "../../components/Cities";
+import Header from "../../components/Header";
 import Content from "../../components/Content";
 import ContinentBanner from "../../components/ContinentBanner";
-import Header from "../../components/Header";
 
 export default function Continent() {
   return (
@@ -11,6 +12,7 @@ export default function Continent() {
 
       <Flex direction="column" maxW="1160px" mx="auto" mb="10" px="1rem">
         <Content />
+        <Cities />
       </Flex>
     </Flex>
   );
